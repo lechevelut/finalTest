0cbd60cdf2c2d0268ffdd19f3a657eea87adbea7
unknown <p1200476@IUTDOUAB2PC12.univ-lyon1.fr>
2015-06-25 08:55:15 +0200

    Passage du deuxième test. TypeReference.

---
+++ FinalTest/TypeReference.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalTest
+{
+    public class TypeReference
+    {
+
+        private int valeur;
+
+        public TypeReference(int i)
+        {
+            this.valeur = i;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            TypeReference typeReferenceObj = obj as TypeReference;
+            if (typeReferenceObj == null)
+                return false;
+            else
+                return valeur.Equals(typeReferenceObj.valeur);
+        }
+
+    }
+}
