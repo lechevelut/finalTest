737c643c5bbf8898cf2902512c8a39e2ce7df859
unknown <p1200476@IUTDOUAB2PC12.univ-lyon1.fr>
2015-06-25 09:33:07 +0200

    Passage du premier test linq. Création de la classe Nombre.

---
+++ FinalTest/Nombres.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalTest
+{
+    public class Nombres
+    {
+        private IEnumerable<KeyValuePair<string, int>> keyValuePairs;
+
+        public Nombres(IEnumerable<KeyValuePair<string, int>> keyValuePairs)
+        {
+            this.keyValuePairs = keyValuePairs;
+        }
+
+        public IEnumerable<int> NombresPairs
+        {
+            get
+            {
+                return keyValuePairs.Where(x => x.Value%2 == 0).Select(x => x.Value);
+            }
+        }
+    }
+}
