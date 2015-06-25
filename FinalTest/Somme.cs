b99a33ecd72bb1a6dec27291528ce69eef7f709a
unknown <p1200476@IUTDOUAB2PC12.univ-lyon1.fr>
2015-06-25 09:06:42 +0200

    Création classe Somme. Passage 4ème test.

---
+++ FinalTest/Somme.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalTest
+{
+    public class Somme
+    {
+        public Somme()
+        {
+        }
+
+        public bool PeutCalculer(string operation)
+        {
+            return operation.Contains("+");
+        }
+
+        public int Calculer(string operation)
+        {
+            int i = int.Parse(operation.Split('+')[0]);
+            int j = int.Parse(operation.Split('+')[1]);
+            return i+j;
+        }
+    }
+}
